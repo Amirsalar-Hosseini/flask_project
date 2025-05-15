dd2ecd5f075cad625c75d5b2e06ed54f87c132a9
amirsalar-hosseini <hosseiniseyedamirsalar@gmail.com>
2025-05-15 11:34:22 +0330

    now you can delete specific user and see all detail users

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy.orm import DeclarativeBase, mapped_column
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String
@@ -29,7 +29,7 @@ with app.app_context():
 
 
 @app.route('/')
-def say_hello():
+def home_page():
     return render_template("home.html", name="amirsalar")
 
 @app.route('/about')
@@ -44,15 +44,22 @@ def create_user():
         db.session.commit()
         return render_template('signup.html')
 
-@app.route('/all_users')
+@app.route('/all-users')
 def show_all_user():
     users = db.session.execute(db.select(User).order_by(User.id)).scalars()
     return render_template('all_users.html', users=users)
 
-@app.route('/specific_user')
-def show_specific_user():
-    user = db.session.execute(db.select(User).where(User.username == 'salar')).scalar()
+@app.route('/specific-user/<string:username>')
+def show_specific_user(username):
+    user = db.session.execute(db.select(User).where(User.username == username)).scalar()
     return render_template('specific_user.html', user=user)
+
+@app.route('/user/<int:user_id>')
+def delete_specific_user(user_id):
+    user = db.session.execute(db.select(User).where(User.id == user_id)).scalar()
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for('home_page'))
 
 if __name__ == '__main__':
     app.run(debug=True)
