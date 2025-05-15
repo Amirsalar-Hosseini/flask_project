1dbdee3104b28a584b9bd4e6f909e6ee51f40500
amirsalar-hoseini <hosseiniseyedamirsalar@gmail.com>
2025-05-15 21:14:06 +0330

    login/register form and routes defined

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,16 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, EmailField
+from wtforms import StringField, PasswordField, BooleanField, EmailField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
-    email = StringField('Email', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('register')
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- apps/users/routes.py
+++ apps/users/routes.py
@@ -1,8 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from apps.users.forms import RegisterForm, LoginForm
 
 blueprint = Blueprint('users', __name__)
 
 
+@blueprint.route('/register')
+def register():
+    form = RegisterForm()
+    return render_template('users/register.html', form=form)
 @blueprint.route('/login')
 def login():
-    return 'login page'
+    form = LoginForm()
+    return render_template('users/login.html', form=form)
