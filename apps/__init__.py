d4df75ab19d3787a9ff82e5c5bce0b5d6af4890d
amirsalar-hosseini <hosseiniseyedamirsalar@gmail.com>
2025-05-19 09:07:06 +0330

    validation register form completed

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from apps.posts.routes import blueprint as posts_blueprint
 from apps.users.routes import blueprint as users_blueprint
+from apps.home.routes import blueprint as home_blueprint
 from apps.extensions import db
 
 
 def register_blueprints(app):
     app.register_blueprint(posts_blueprint)
     app.register_blueprint(users_blueprint)
+    app.register_blueprint(home_blueprint)
 app = Flask(__name__)
 register_blueprints(app)
 app.config.from_object('config.DevelopmentConfig')
--- apps/users/routes.py
+++ apps/users/routes.py
@@ -1,13 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from apps.users.forms import RegisterForm, LoginForm
 from apps.users.models import User
+from apps.extensions import hashing, db
 
 blueprint = Blueprint('users', __name__)
 
 
-@blueprint.route('/register')
+@blueprint.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        print('helooooooooooooooooooooooooooooooooooo')
+        hashed_password = hashing.hash_value(form.password.data)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('users.login'))
     return render_template('users/register.html', form=form)
 @blueprint.route('/login')
 def login():
