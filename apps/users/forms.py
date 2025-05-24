c0228db16fff92c21e3bb23879c27c944951212f
amirsalar-hosseini <hosseiniseyedamirsalar@gmail.com>
2025-05-24 14:49:28 +0330

    add validation form that you can show above register form

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,14 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, EmailField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
-
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from apps.extensions import db
+from apps.users.models import User
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
-    email = EmailField('Email', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('register')
+
+
+    def validate_email(self, field):
+        user = db.session.execute(db.select(User).where(User.email == field.data)).scalar()
+        if user:
+            raise ValidationError('Email already registered.')
 
 
 class LoginForm(FlaskForm):
